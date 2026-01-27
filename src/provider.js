d90e8a34e02e589e3090623358dad9037016f3ea
Antoniy Chonkov <antoniy@chonkov.net>
2026-01-27 15:37:23 +0000

    Fix

--- src/provider.js
+++ src/provider.js
@@ -1,4 +1,14 @@
 const axios = require('axios');
+axios.interceptors.request.use(request => {
+  console.log('Request', JSON.stringify(request, null, 2))
+  return request
+})
+    
+axios.interceptors.response.use(response => {
+  console.log('Response:', JSON.stringify(response, null, 2))
+  return response
+})
+
 const NodeCache = require('node-cache');
 
 const cache = new NodeCache({
