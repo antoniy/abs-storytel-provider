3933be356b71a43df0e3c09664f7baccd0fbe268
Antoniy Chonkov <antoniy@chonkov.net>
2026-01-27 15:46:24 +0000

    Fix

--- src/provider.js
+++ src/provider.js
@@ -315,6 +315,7 @@ class StorytelProvider {
                     request_locale: locale
                 },
                 headers: {
+                    'Accept': 'application/json',
                     'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
                 }
             })
